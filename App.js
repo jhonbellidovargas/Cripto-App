e22918dff461385cf9e9084a11589ff650c20de4
Jhon Bellido Vargas <jhon.bellido.27@unsch.edu.pe>
2022-07-27 16:17:46 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import CoinsStack from './src/components/coins/CoinsStack';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <CoinsStack />
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class CoinsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>CoinsScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default CoinsScreen;
